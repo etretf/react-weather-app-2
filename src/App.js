8ede327e50f2f8ad23d12704db744747ff53d677
etretf <77692774+etretf@users.noreply.github.com>
2021-04-01 10:06:30 -0400

    Added github url

--- src/App.js
+++ src/App.js
@@ -3,7 +3,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-    Weather App
+    <h1>Weather App</h1>
+    <footer>
+      <a href="https://github.com/etretf/react-weather-app-2" target="_blank">Open source code</a>
+      <span> by Emma Souannhaphanh</span>
+    </footer>
     </div>
   );
 }
